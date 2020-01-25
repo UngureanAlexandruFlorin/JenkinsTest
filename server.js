5ce9da59580b64fa0d53bec19778cbf48d49c354
debian <ungurean.alexandru.florin@gmail.com>
2020-01-25 16:23:37 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 
 http.createServer((req, res) => {
-	res.write("<h1>It's working!</h1>");
+	res.write("<h1>It's finally working!</h1>");
 	res.end();
 }).listen(3500);
